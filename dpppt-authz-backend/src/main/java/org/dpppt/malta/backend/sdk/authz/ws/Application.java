499a3f866c4e0f6899bf21ee59f24fd7a904aa2f
Malcolm Spiteri <malcolm.spiteri@gov.mt>
2020-09-25 12:41:59 +0200

    Exclude spring security from auto-config

--- dpppt-authz-backend/src/main/java/org/dpppt/malta/backend/sdk/authz/ws/Application.java
+++ dpppt-authz-backend/src/main/java/org/dpppt/malta/backend/sdk/authz/ws/Application.java
@@ -11,7 +11,9 @@
 package org.dpppt.malta.backend.sdk.authz.ws;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -19,7 +21,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @ComponentScan(basePackages = {"org.dpppt.malta.backend.sdk.authz.ws.config"})
-@EnableAutoConfiguration
+@EnableAutoConfiguration(exclude = { SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class })
 @EnableWebMvc
 @EnableScheduling
 public class Application {
